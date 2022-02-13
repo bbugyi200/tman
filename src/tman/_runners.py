6eb34d6dee1ec8c1cab705f0207cffd94d185759
Bryan Bugyi <bryanbugyi34@gmail.com>
2022-02-13 15:26:43 -0500

    Run shutdown commands when exiting via a SIGTERM

--- src/tman/_runners.py
+++ src/tman/_runners.py
@@ -2,8 +2,12 @@
 
 from __future__ import annotations
 
+import atexit
 import os
+import signal
+import sys
 import time
+from types import FrameType
 from typing import List
 
 import clack
@@ -33,27 +37,32 @@ def run_start(cfg: StartConfig) -> int:
     """Runner for the `tman start` command."""
     cfg.torrent_bucket.parent.mkdir(parents=True, exist_ok=True)
 
-    try:
-        execute_commands(*cfg.startup_commands)
-        if cfg.torrent_bucket.is_file():
-            add_cmds = []
-            for line in cfg.torrent_bucket.open():
-                download_dir, magnet_url = line.split(" ", maxsplit=1)
-                cmd = cfg.add_command.format(
-                    download_dir=download_dir, magnet_url=magnet_url
-                )
-                add_cmds.append(cmd)
+    execute_commands(*cfg.startup_commands)
+    if cfg.torrent_bucket.is_file():
+        add_cmds = []
+        for line in cfg.torrent_bucket.open():
+            download_dir, magnet_url = line.split(" ", maxsplit=1)
+            cmd = cfg.add_command.format(
+                download_dir=download_dir, magnet_url=magnet_url
+            )
+            add_cmds.append(cmd)
 
-            execute_commands(*add_cmds)
-            cfg.torrent_bucket.unlink()
+        execute_commands(*add_cmds)
+        cfg.torrent_bucket.unlink()
 
-        logger.info(
-            "Downloading / seeding torrents for configured amount of time.",
-            seconds=cfg.runtime,
-        )
-        time.sleep(cfg.runtime)
-    finally:
-        execute_commands(*cfg.shutdown_commands)
+    logger.info(
+        "Downloading / seeding torrents for configured amount of time.",
+        seconds=cfg.runtime,
+    )
+
+    def signal_handler(signum: int, frame: FrameType | None) -> None:
+        del frame
+        sys.exit(128 + signum)
+
+    atexit.register(execute_commands, *cfg.shutdown_commands)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    time.sleep(cfg.runtime)
 
     return 0
 
